dfbfb5d4a684929fa626e4c6ca55fe21c184457e
Bourbs <Bourbonplaytime@gmail.com>
2020-01-27 09:17:38 -0800

    Basic yatzy simulator

---
+++ dice.py
@@ -0,0 +1,47 @@
+import random
+
+class Dice:
+    def __init__(self, sides=2, value=0):
+        if not sides >= 2:
+            raise ValueError("Must have at least 2 sides")
+        if not isinstance(sides, int):
+            raise ValueError("Sides must be a whole number")
+        if type(value) is int:
+            self.value = value or random.randint(1, sides)
+        else:
+            raise ValueError("Value must be a whole number")
+        
+    def __int__(self):
+        return self.value
+
+    def __eq__(self, other):
+        return int(self) == other
+
+    def __ne__(self, other):
+        return int(self) != other
+
+    def __gt__(self, other):
+        return int(self) > other
+
+    def __lt__(self, other):
+        return int(self) < other
+
+    def __ge__(self, other):
+        return int(self) > other or int(self) == other
+
+    def __le__(self, other):
+        return int(self) < other or int(self) == other 
+
+    def __add__(self, other):
+        return int(self) + other
+
+    def __radd__(self, other):
+        return int(self) + other
+
+    def __repr__(self):
+        return str(self.value)
+
+
+class D6(Dice):
+    def __init__(self, value=0):
+        super().__init__(sides=6, value=value)
